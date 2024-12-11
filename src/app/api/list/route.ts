1caa1f3c7e107e2c97207f8c0404fe2709f62b38
pengyuanwang <wpy1174555847@outlook.com>
2024-12-11 18:55:56 +0800

    [2024-12-11] update apple-icon

--- src/app/api/list/route.ts
+++ src/app/api/list/route.ts
@@ -2,11 +2,18 @@ import { NextResponse } from "next/server";
 import fs from "fs/promises";
 import type { LayoffsItem } from "@/app/lib/type";
 
-export async function GET() {
-  const file = await fs.readFile(
-    process.cwd() + "/src/data/json/list.json",
-    "utf-8"
-  );
-  const lists: LayoffsItem[] = JSON.parse(file);
-  return NextResponse.json(lists);
+const file = await fs.readFile(
+  process.cwd() + "/src/data/json/list.json",
+  "utf-8"
+);
+
+const lists: LayoffsItem[] = JSON.parse(file);
+
+export function GET() {
+  return NextResponse.json(lists, {
+      status: 200,
+      headers: {
+        "Cache-Control": "public, max-age=60, stale-while-revalidate=300",
+      },
+  });
 }
